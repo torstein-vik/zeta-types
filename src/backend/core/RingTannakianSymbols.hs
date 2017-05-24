ee7a850edf69ae1f2e7207c051b02e58341a1e0e
Torstein Vik <torsteinv64@gmail.com>
2017-05-25 00:08:50 +0200

    Updated semantics for RingTannakianSymbols

--- src/backend/core/RingTannakianSymbols.hs
+++ src/backend/core/RingTannakianSymbols.hs
@@ -11,6 +11,8 @@ module RingTannakianSymbols (
   ) where
 
 import Control.Monad
+import Data.Maybe
+
 
 import TannakianSymbols
 import Algebra
@@ -18,18 +20,18 @@ import Algebra
 import qualified Prelude
 import Prelude hiding ((+),(-),(*),(^),(/), negate)
 
-trace :: (CMult m, CAdd m) => TS m -> m
+trace :: (CMult m, CAdd m, CZModule m) => TS m -> m
 trace symbol = foldr (+) zero $ fmap (uncurry (*#)) $ unSymbol symbol
 
-pointCounts :: (Eq m, CMult m, CAdd m) => TS m -> [m]
+pointCounts :: (Eq m, CMult m, CAdd m, CZModule m) => TS m -> [m]
 pointCounts m = [e] ++ do
                 k <- [1..]
                 return $ trace $ psi k m
 
-bellCoeffs :: (Eq m, CMult m, CAdd m, CQAlgebra m) => TS m -> [m]
+bellCoeffs :: (Eq m, CMult m, CAdd m, CZModule m, CPartialQModule m) => TS m -> [m]
 bellCoeffs = bellAntiDerivative . pointCounts
 
-bellDerivative :: (CAdd m, CMult m, CZAlgebra m) => [m] -> [m]
+bellDerivative :: (CAdd m, CMult m, CZModule m) => [m] -> [m]
 bellDerivative lst = memo where
                         memoList = (memo !!)
                         memo = map (bd (memoList)) (fmap (\(i ,_) -> i) (zip [0..] lst))
@@ -38,11 +40,11 @@ bellDerivative lst = memo where
                         bd bdP 1 = lst !! 1
                         bd bdP i = (lst !! i) *# i - foldr (+) zero (fmap (\j -> bdP (i - j) * (lst !! j)) [1..i - 1])
                         
-bellAntiDerivative :: (CAdd m, CMult m, CQAlgebra m) => [m] -> [m]
+bellAntiDerivative :: (CAdd m, CMult m, CPartialQModule m) => [m] -> [m]
 bellAntiDerivative lst = memo where
                         memoList = (memo !!)
                         memo = map (bd (memoList)) (fmap (\(i ,_) -> i) (zip [0..] lst))
                     
                         bd bdP 0 = e
                         bd bdP 1 = lst !! 1
-                        bd bdP i = ((lst !! i) + foldr (+) zero (fmap (\j -> bdP (i - j) * (lst !! j)) [1..i - 1])) /# i
+                        bd bdP i = fromJust $ ((lst !! i) + foldr (+) zero (fmap (\j -> bdP (i - j) * (lst !! j)) [1..i - 1])) /# (toInteger i)
